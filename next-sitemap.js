14a5cadcaffc9b36b56016588ee89ed54138d25e
Tabulator <105991140+Tabulater@users.noreply.github.com>
2025-03-19 21:14:55 -0400

    Remove unnecessary files and clean up project directory

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://aashrithrajtatipamula.vercel.app',
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  outDir: './public', // Specify the output directory for the sitemap files
+  // ...other options
+}; 
